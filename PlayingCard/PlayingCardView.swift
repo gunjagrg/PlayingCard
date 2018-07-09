41148f8e3d825ed4614e64b9c34bf2e14fee6fbf
Gunja's Github <gunjagurung@gunjas-imac.local>
2018-07-09 01:20:11 -0400

    contentMode = .redraw for UIBezierPath drawing practice.

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -9,8 +9,11 @@
 import UIKit
 
 class PlayingCardView: UIView {
+    
 
     override func draw(_ rect: CGRect) {
+        self.contentMode = .redraw
+        
         let path = UIBezierPath()
         path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
                     radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
@@ -19,6 +22,7 @@ class PlayingCardView: UIView {
         UIColor.red.setStroke()
         path.stroke()
         path.fill()
+        
     }
  
 
