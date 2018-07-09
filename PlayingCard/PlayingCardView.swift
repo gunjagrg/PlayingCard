9045d1918777ba74881e4a5ed8fd083e72716535
Gunja's Github <gunjagurung@gunjas-imac.local>
2018-07-09 01:02:54 -0400

    Drawing practice using context (UIGraphicsGetCurrentContext).

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -12,6 +12,16 @@ class PlayingCardView: UIView {
 
     override func draw(_ rect: CGRect) {
         
+        let context = UIGraphicsGetCurrentContext()!
+        context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
+            radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        context.setLineWidth(5.0)
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        context.strokePath()
+        context.fillPath() // Fill path doesn't work as strokePath consumes the path (while using context).
+                           //BezierPath, on the other hand, doesn't consume it.
+        
     }
  
 
