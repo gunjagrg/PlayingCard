7fa7eb185d6e2914803e7ee64c0c027b066b8df7
Gunja's Github <gunjagurung@gunjas-imac.local>
2018-07-09 01:09:25 -0400

    Drawing practice with UIBezierPath.

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -11,17 +11,14 @@ import UIKit
 class PlayingCardView: UIView {
 
     override func draw(_ rect: CGRect) {
-        
-        let context = UIGraphicsGetCurrentContext()!
-        context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
-            radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        context.setLineWidth(5.0)
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
+                    radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
         UIColor.green.setFill()
         UIColor.red.setStroke()
-        context.strokePath()
-        context.fillPath() // Fill path doesn't work as strokePath consumes the path (while using context).
-                           //BezierPath, on the other hand, doesn't consume it.
-        
+        path.stroke()
+        path.fill()
     }
  
 
